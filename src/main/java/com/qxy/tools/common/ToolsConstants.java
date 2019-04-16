31e6ce99fd29cf8dbd4b5cc8c9c9dd480f8ff94d
qinxiaoyun <qinxiaoyun001@lianjia.com>
2019-04-16 15:25:00 +0800

    添加 traceId 参数信息，生成方式 UUID

---
+++ src/main/java/com/qxy/tools/common/ToolsConstants.java
@@ -0,0 +1,15 @@
+package com.qxy.tools.common;
+
+/**
+ * @Description: 类信息描述
+ * @Author: qinxiaoyun001@lianjia.com
+ * @Date: 2019/4/16 3:22 PM
+ * @Version: 1.0
+ */
+public class ToolsConstants {
+
+
+    public static final String UNDERLINE = "_";
+
+    public static final String EMPYT_STRING = "";
+}
--- src/main/java/com/qxy/tools/controller/AuthController.java
+++ src/main/java/com/qxy/tools/controller/AuthController.java
@@ -1,12 +1,15 @@
 package com.qxy.tools.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.qxy.tools.common.ToolsConstants;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 /**
  * @Description: 类信息描述
@@ -24,14 +27,16 @@ public class AuthController {
     }
 
     private final static String SPLIT = "&";
+
     /**
      * 按照规则加密,该方法不能随意变更
+     *
      * @param appkey
      * @param token
      * @param timestamp
      * @return
      */
-    public static String encode(String appkey, String token, long timestamp){
+    public static String encode(String appkey, String token, long timestamp) {
         StringBuilder codeBuilder = new StringBuilder();
 
         codeBuilder.append(appkey);
@@ -50,10 +55,11 @@ public class AuthController {
     public JSONObject getAuthByAppkeyAndSecret(@RequestParam("appKey") String appKey,
                                                @RequestParam("secret") String secret) {
         JSONObject result = new JSONObject();
-        result.put("appKey",appKey);
+        result.put("appKey", appKey);
         Long timestamp = System.currentTimeMillis();
-        result.put("timestamp",timestamp);
-        result.put("signCode",encode(appKey,secret,timestamp));
+        result.put("timestamp", timestamp);
+        result.put("signCode", encode(appKey, secret, timestamp));
+        result.put("traceId", UUID.randomUUID().toString().replaceAll(ToolsConstants.UNDERLINE, ToolsConstants.EMPYT_STRING));
         return result;
     }
 }
