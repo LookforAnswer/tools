7eee4081f6528fcfabf5adc402fe0c29067b8768
qinxiaoyun <qinxiaoyun001@lianjia.com>
2019-04-16 15:46:02 +0800

    fix shell

--- build.sh
+++ build.sh
@@ -1,82 +1,30 @@
 #!/bin/bash
 
+#!/bin/sh
+echo update source code...
+git branch
+git pull
 
+echo stop application...
 
+PID=$(ps -ef | grep tools-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+    echo application is already stopped.
+else
+    echo killing $PID.
+    kill $PID
+    echo done.
+fi
 
-#这里可替换为你自己的执行程序，其他代码无需更改
-APP_NAME=tools-0.0.1-SNAPSHOT.jar
+echo package application...
 
-#使用说明，用来提示输入参数
-usage() {
-    echo "Usage: sh 执行脚本.sh [start|stop|restart|status]"
-    exit 1
-}
+mvn -U -T 1C clean package -pl tools -am
 
-#检查程序是否在运行
-is_exist(){
-  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
-  #如果不存在返回1，存在返回0
-  if [ -z "${pid}" ]; then
-   return 1
-  else
-    return 0
-  fi
-}
+echo start application...
 
-#启动方法
-start(){
-  is_exist
-  if [ $? -eq "0" ]; then
-    echo "${APP_NAME} is already running. pid=${pid} ."
-  else
-    nohup java -jar $APP_NAME > /dev/null 2>&1 &
-  fi
-  tail -f nohup.out
-}
+cd target
 
-#停止方法
-stop(){
-  is_exist
-  if [ $? -eq "0" ]; then
-    kill -9 $pid
-  else
-    echo "${APP_NAME} is not running"
-  fi
-}
+nohup java -jar $APP_NAME > /dev/null 2>&1 &
 
-#输出运行状态
-status(){
-  mvn -U -T 1C clean package
-
-  is_exist
-  if [ $? -eq "0" ]; then
-    echo "${APP_NAME} is running. Pid is ${pid}"
-  else
-    echo "${APP_NAME} is NOT running."
-  fi
-}
-
-#重启
-restart(){
-  stop
-  start
-}
-
-#根据输入参数，选择执行对应方法，不输入则执行使用说明
-case "$1" in
-  "start")
-    start
-    ;;
-  "stop")
-    stop
-    ;;
-  "status")
-    status
-    ;;
-  "restart")
-    restart
-    ;;
-  *)
-    usage
-    ;;
-esac
+tail -f nohup.out
